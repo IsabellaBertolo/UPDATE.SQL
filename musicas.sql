3f7408def9e6fed6bd2a69f7f0f36007fd605fba
Isabella Bertolo dos Santos <125597510+IsabellaBertolo@users.noreply.github.com>
2024-03-28 15:29:31 -0300

    musicas.sql

---
+++ musicas.sql
@@ -0,0 +1,24 @@
+INSERT INTO musicasFavoritas (id, musica, artista, idioma, duracaoMin) VALUES
+    (1,'Summertime Sadness', 'Lana del Rey', 'EN', '4'),
+    (2,'Vagabundo e a dama', 'Oriente', 'PT-BR', '4'),
+    (3,'Habits of My Heart', 'Jaymes Young', 'EN', '3'),
+    (4,'A droga do amor', 'Ari', 'PT-BR', '3'),
+    (5,'Pontos de Exclamação', 'Jovem Dionisio', 'PT-BR', '3'),
+    (6,'Cardigan', 'Taylor Swift', 'Inglês', '3'),
+    (7,'Nada Contra (ciúme)', 'Clarissa', 'PT-BR', '3'),
+    (8,'Save Your Tears', 'The Weeknd', 'EN', '3'),
+    (9,'Garupa de moto amarela', 'Tim Bernardes', 'PT-BR', '3'),
+    (10,'Anos luz', 'Matue', 'PT-BR', '3'),
+    (11,'Vai Me Dando Corda', 'Grupo Menos é Mais', 'PT-BR', '3'),
+    (12,'Sorri,Sou rei', 'Natiruts', 'PT-BR', '5'),
+    (13,'Can’t Catch Me Now', 'Olivia Rodrigo', 'Inglês', '3'),
+    (14,'Burguesia', 'Seu Jorge', 'PT-BR', '4'),
+    (15,'Penhasco', 'Luisa Sonsa', 'PT-BR', '3'),
+    (16,'Céu Azul ', 'Charlie Brown Jr', 'PT-BR', '3'),
+    (17,'Piloto', 'Flora Matos', 'PT-BR', '3:'),
+    (18,'Linda Louca e Mimada', 'Oriente Acústico', 'PT-BR', '3'),
+    (19,'Onde Anda Você', 'Tiago Nacarato', 'PT-BR', '3'),
+    (20,'I Wanna Be Yours', 'Arctic Monkeys', 'PT-BR', '3'),
+    
+   
+    
